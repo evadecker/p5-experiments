f43329b9aea43ab98ceb807d570cd9443de96999
Eva Decker <itsevadecker@gmail.com>
2024-06-29 19:26:07 -0400

    Update site title and URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://mysite.com",
+  site: "https://experiments.eva.town",
   devToolbar: {
     enabled: false,
   },
